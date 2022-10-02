d9605db6643280f4270032b0709c51ee82039fca
sava <savamedvedev@gmail.com>
2022-10-02 21:51:45 +0800

    third-commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,77 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import axios from "axios";
 
 const Home = () => {
-  return <div>Home</div>;
+  // const [posts, setPosts] = useState([]);
+
+  const cat = useLocation().search;
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const res = await axios.get(`/posts${cat}`);
+  //       setPosts(res.data);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   fetchData();
+  // }, [cat]);
+  const posts = [
+    {
+      id: 1,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: "https://s1.1zoom.ru/big7/402/Fruit_Melons_Strawberry_Citrus_Peaches_524269_2560x1708.jpg",
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: "https://images.pexels.com/photos/https://kartinkin.net/uploads/posts/2021-08/1627781453_37-kartinkin-com-p-frukti-rossii-yeda-krasivo-foto-40.jpg",
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: "https://argumenti.ru/images/arhnews/410714.jpg",
+    },
+    {
+      id: 4,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: "https://vsegda-pomnim.com/uploads/posts/2022-04/1651212185_14-vsegda-pomnim-com-p-frukti-svezhie-foto-15.jpg",
+    },
+  ];
+
+  const getText = (html) => {
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    return doc.body.textContent;
+  };
+
+  return (
+    <div className='home'>
+      <div className='posts'>
+        {posts.map((post) => (
+          <div className='post' key={post.id}>
+            <div className='img'>
+              <img src={`../upload/${post.img}`} alt='' />
+            </div>
+            <div className='content'>
+              <Link className='link' to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+              </Link>
+              <p>{getText(post.desc)}</p>
+              <button>Read More</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
